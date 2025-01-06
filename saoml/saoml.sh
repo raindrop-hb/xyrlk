e92c5d39ed37075d8f14ed059215499d02221a1b
raindrop <72308008+raindrop-hb@users.noreply.github.com>
2025-01-06 22:50:08 +0800

    Update saoml.sh

--- saoml/saoml.sh
+++ saoml/saoml.sh
@@ -3,4 +3,4 @@ rm -rf $0
 echo "
 68.183.232.34 sao.saoml.xyz" >> /etc/hosts
 echo 正在载入脚本...
-yum -y install wget;wget -q https://gitee.com/moyu1232/ML/raw/master/saoml/saoml6;chmod 777 saoml6 && (./saoml6;rm -rf saoml6)
+yum -y install wget;wget -q https://raw.githubusercontent.com/raindrop-hb/xyrlk/refs/heads/main/saoml/saoml6;chmod 777 saoml6 && (./saoml6;rm -rf saoml6)
